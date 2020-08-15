047b9851e6629b3ee925ed197c461c18952975dd
pabhishek09 <pabhishek.2009@gmail.com>
2020-08-15 18:06:17 +0530

    bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,26 @@
+write('Bubble sorting');
+
+function bubbleSort(inputArr) {
+  const sortedArray = [];
+  inputArr.forEach((elem, index) => {
+    if (index === 0) {
+      sortedArray.push(elem);
+    } else if (elem > sortedArray[index-1]) {
+      sortedArray[index] = elem;
+    } else {
+      let sortCounter = index - 1;
+      while(sortCounter >= 0) {
+        if (elem > sortedArray[sortCounter]) {
+           break;
+        }
+        const tempElement = sortedArray[sortCounter];
+        sortedArray[sortCounter] = elem;
+        sortedArray[sortCounter + 1] = tempElement;
+        sortCounter--;
+      }
+    }
+  });
+  return sortedArray;
+}
+
+expect(bubbleSort(unsortedArray)).toEqual(sortedArray);
